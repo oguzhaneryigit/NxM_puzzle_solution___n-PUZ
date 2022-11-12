cb948eb0fe6a81446132983bbe2c49240e208802
oguzhan eryigit <oguzhanerygt@gmail.com>
2022-11-12 14:17:10 +0300

    commit 3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy
 import random
 
@@ -27,6 +29,15 @@ def findZero(matrix):
     print("0 bulunamadı")
     return (-1, -1)
 
+def findElement(matrix,n):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == n:
+                return (i, j)
+
+    # normal şartlarda bura çalışmamalı
+    print("0 bulunamadı")
+    return (-1, -1)
 
 def findPossibleMoves(matrix, previusMovement=""):
     x, y = findZero(matrix)
@@ -82,8 +93,38 @@ def defineNxMmatrix(n, m):
     # print(matrix)
     # printMatrix(matrix)
     return matrix
+def defineMatrix(n):
+    solvedMatrix= defineDestinationMatrix(n)
+    for i in range(619):
+        moves=findPossibleMoves(solvedMatrix)
+        nextMove=random.choice(moves)
+        zeroX,zeroY=findZero(solvedMatrix)
+        if "l" ==nextMove:
+            solvedMatrix = swap(solvedMatrix, zeroX,zeroY, zeroX, zeroY - 1)
 
+        if "r" ==nextMove:
+            solvedMatrix = swap(solvedMatrix, zeroX, zeroY,zeroX, zeroY + 1)
 
+        if "u" ==nextMove:
+            solvedMatrix = swap(solvedMatrix, zeroX, zeroY, zeroX - 1, zeroY)
+
+        if "d" ==nextMove:
+            solvedMatrix = swap(solvedMatrix, zeroX, zeroY, zeroX + 1, zeroY)
+
+    return solvedMatrix
+def defineDestinationMatrix(n):
+    n = math.ceil(math.sqrt(n + 1))
+    matrix = []  # define empty matrix
+    numbers = list(range(n * n))
+    k = 1
+    for i in range(n):  # total row is 3
+        row = []
+        for j in range(n):  # total column is 3
+            row.append(k)
+            k += 1
+        matrix.append(row)
+    matrix[n-1][n-1]=0
+    return matrix
 def verifyMatrix(matrix):
     temp = 1
     for i in matrix:
@@ -107,7 +148,7 @@ def verifyMatrix(matrix):
 
 
 class Node:
-    def __init__(self, matrix, prevMovement=""):
+    def __init__(self, matrix,depth, prevMovement=""):
         self.matrix = matrix
         self.prevMovement = prevMovement
         self.moves = findPossibleMoves(matrix, prevMovement)
@@ -117,6 +158,11 @@ class Node:
         self.upper = None
         self.down = None
         self.isValidGraph = verifyMatrix(self.matrix)
+        self.depth =depth
+        self.destMatrix = defineDestinationMatrix(len(matrix)*len(matrix)-1)
+        self.cost= calculateHeuristic(matrix,self.destMatrix)
+        self.astarcost=self.cost+self.depth
+
 
     def hasValidChild(self):
 
@@ -140,19 +186,19 @@ class Node:
     def addChildren(self):
         tempNodeList = []
         if "l" in self.moves:
-            self.left = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX, self.zeroY - 1), "l")
+            self.left = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX, self.zeroY - 1), self.depth+1,"l")
             tempNodeList.append(self.left)
 
         if "r" in self.moves:
-            self.right = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX, self.zeroY + 1), "r")
+            self.right = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX, self.zeroY + 1),self.depth+1, "r")
             tempNodeList.append(self.right)
 
         if "u" in self.moves:
-            self.upper = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX - 1, self.zeroY), "u")
+            self.upper = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX - 1, self.zeroY), self.depth+1,"u")
             tempNodeList.append(self.upper)
 
         if "d" in self.moves:
-            self.down = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX + 1, self.zeroY), "d")
+            self.down = Node(swap(self.matrix, self.zeroX, self.zeroY, self.zeroX + 1, self.zeroY), self.depth+1,"d")
             tempNodeList.append(self.down)
 
         return tempNodeList
@@ -177,6 +223,9 @@ class Node:
             printMatrix(self.down.matrix)
 
 
+
+
+
 def bfsWithAdder(root):
     queue = []
     queue.append(root)
@@ -194,39 +243,184 @@ def bfsWithAdder(root):
             break
         queue.extend(children)
 
+def greedyBestSearch(root):
+    sortedQueue=[]
+    sortedQueue.append(root)
+    counter = 0
+    while (len(sortedQueue) > 0):
+        indexNode = sortedQueue.pop(0)
+        printMatrix(indexNode.matrix)
+        print(indexNode.moves)
+        counter += 1
+        print(counter)
+        children = indexNode.addChildren()
+        if type(indexNode.hasValidChild()) == Node:
+            print(f"SONUÇ {counter}. ADIMDA BULUNDU")
+            printMatrix(indexNode.hasValidChild().matrix)
+            break
+        for i in range(len(children)):
+            for j in range(len(children)):
+                if children[i].cost<children[j].cost:
+                    temp=children[i]
+                    children[i]=children[j]
+                    children[j]=temp
+
+        if len(sortedQueue)==0:
+            sortedQueue.extend(children)
+        else:
+            for i in range(len(children)):
+                added=False
+                for j in range(len(sortedQueue)):
+                    if children[i].cost<sortedQueue[j].cost:
+                        sortedQueue.insert(j,children[i])
+                        added=True
+                        break
+                if added==False:
+                    sortedQueue.append(children[i])
+
+def uniformCostSearch(root):
+    sortedQueue=[]
+    sortedQueue.append(root)
+    counter = 0
+    while (len(sortedQueue) > 0):
+        indexNode = sortedQueue.pop(0)
+        printMatrix(indexNode.matrix)
+        print(indexNode.moves)
+        counter += 1
+        print(counter)
+        children = indexNode.addChildren()
+        if type(indexNode.hasValidChild()) == Node:
+            print(f"SONUÇ {counter}. ADIMDA BULUNDU")
+            printMatrix(indexNode.hasValidChild().matrix)
+            break
+        for i in range(len(children)):
+            for j in range(len(children)):
+                if children[i].depth<children[j].depth:
+                    temp=children[i]
+                    children[i]=children[j]
+                    children[j]=temp
+
+        if len(sortedQueue)==0:
+            sortedQueue.extend(children)
+        else:
+            for i in range(len(children)):
+                added=False
+                for j in range(len(sortedQueue)):
+                    if children[i].depth<sortedQueue[j].depth:
+                        sortedQueue.insert(j,children[i])
+                        added=True
+                        break
+                if added==False:
+                    sortedQueue.append(children[i])
+
+def aStar(root):
+    sortedQueue=[]
+    sortedQueue.append(root)
+    counter = 0
+    while (len(sortedQueue) > 0):
+        indexNode = sortedQueue.pop(0)
+        printMatrix(indexNode.matrix)
+        print(indexNode.moves)
+        counter += 1
+        print(counter)
+        children = indexNode.addChildren()
+        if type(indexNode.hasValidChild()) == Node:
+            print(f"SONUÇ {counter}. ADIMDA BULUNDU")
+            printMatrix(indexNode.hasValidChild().matrix)
+            break
+        for i in range(len(children)):
+            for j in range(len(children)):
+                if children[i].astarcost<children[j].astarcost:
+                    temp=children[i]
+                    children[i]=children[j]
+                    children[j]=temp
+
+        if len(sortedQueue)==0:
+            sortedQueue.extend(children)
+        else:
+            for i in range(len(children)):
+                added=False
+                for j in range(len(sortedQueue)):
+                    if children[i].astarcost<sortedQueue[j].astarcost:
+                        sortedQueue.insert(j,children[i])
+                        added=True
+                        break
+                if added==False:
+                    sortedQueue.append(children[i])
+
+def depthLimitedSearch(root,limit):
+    stack=[]
+    stack.append(root)
+    counter = 0
+    while (len(stack) > 0):
+        indexNode = stack.pop()
+        printMatrix(indexNode.matrix)
+        print(indexNode.moves)
+        counter += 1
+        print(counter)
+        children = indexNode.addChildren()
+        if type(indexNode.hasValidChild()) == Node:
+            print(f"SONUÇ {counter}. ADIMDA BULUNDU.  derinlik={indexNode.depth}")
+            printMatrix(indexNode.hasValidChild().matrix)
+            break
+        if children[0].depth>limit:
+            continue
+        else:
+            stack.extend(children)
+
+    if len(stack)==0:
+        print("loop has broken and result could not  found")
+
+
+def depthFirstSearch(root):
+    stack = []
+    stack.append(root)
+    counter = 0
+    while (len(stack) > 0):
+        indexNode = stack.pop()
+        printMatrix(indexNode.matrix)
+        print(indexNode.moves)
+        counter += 1
+        print(counter)
+        children = indexNode.addChildren()
+        if type(indexNode.hasValidChild()) == Node:
+            print(f"SONUÇ {counter}. ADIMDA BULUNDU.  derinlik={indexNode.depth}")
+            printMatrix(indexNode.hasValidChild().matrix)
+            break
+        stack.extend(children)
+
+def calculateHeuristic(matrix,destMatrix):
+    n= len(matrix)
+
+    cost=0
+    for i in range(n):
+        for j in range(n):
+            value=matrix[i][j]
+            desti,destj = findElement(destMatrix,value)
+            cost+= math.fabs(desti-i)+math.fabs(destj-j)
+    return cost
 
 def main():
-    # matrix = defineNxMmatrix(2, 3)
-    matrix = [
-        [7, 2, 4],
-        [5, 0, 6],
-        [8, 3, 1]
-    ]
+    n=8
+    matrix = defineMatrix(n)
+    # # matrix = [
+    # #     [5, 2, 6],
+    # #     [1, 4, 3],
+    # #     [0, 8, 7]
+    # # ]
+    # printMatrix(defineDestinationMatrix(n))
+    printMatrix(matrix)
+    input("hesaplamaya başlamak için herhangi bir tuşa basın")
     verifyMatrix(matrix)
 
-    root = Node(matrix)
+    root = Node(matrix,0)
+    print()
+    # aStar(root)
     bfsWithAdder(root)
-
-
+    # depthLimitedSearch(root,20)
+    # depthFirstSearch(root)
+    # uniformCostSearch(root)
 if __name__ == '__main__':
     main()
 
-# For simplicity, you can use a Queue to perform BFS non recursively. You need two data structures here.
-#
-# A Queue to maintain the BFS order.
-# List item hash table (or set) to look for duplicates.
-# This is the algorithm:
-#
-# Enqueue the initial point on the graph into the queue and also the hash table.
-# If the queue is not empty
-#
-# Dequeue from the queue.
-# çocuklarını ekle
-# çocuklar içinde true var mı hesapla
-# true yok ise
-# çocukları ekle
-# true var ise çocukları ekle ve  diğer düğümlere çocuk eklemeyi  iptal et
-# En queue all neighbors of the dequeued element to the queue and insert them into the set if they are not already present in the set.
-# Print (/access/process) the dequeued element.
-# Repeat steps 2 through 4 until the queue is exhausted.
-# You can find many examples and optimizations online. Eg:
+
